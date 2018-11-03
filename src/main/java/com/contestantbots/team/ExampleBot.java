a717f191034f21fdd84aeff90f49d36b3c4c3237
James Chalk's Laptop <james.e.chalk@gmail.com>
2018-11-03 14:49:45 +0000

    end of tut 1

--- src/main/java/com/contestantbots/team/ExampleBot.java
+++ src/main/java/com/contestantbots/team/ExampleBot.java
@@ -1,13 +1,16 @@
 package com.contestantbots.team;
 
 import com.contestantbots.util.GameStateLogger;
+import com.contestantbots.util.MoveImpl;
 import com.scottlogic.hackathon.client.Client;
 import com.scottlogic.hackathon.game.Bot;
+import com.scottlogic.hackathon.game.Direction;
 import com.scottlogic.hackathon.game.GameState;
 import com.scottlogic.hackathon.game.Move;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ExampleBot extends Bot {
     private final GameStateLogger gameStateLogger;
@@ -20,7 +23,22 @@ public class ExampleBot extends Bot {
     @Override
     public List<Move> makeMoves(final GameState gameState) {
         gameStateLogger.process(gameState);
-        return new ArrayList<>();
+        List<Move> moves = new ArrayList<>();
+
+        moves.addAll(doExplore(gameState));
+
+        return moves;
+    }
+
+    private List<Move> doExplore(final GameState gameState) {
+        List<Move> exploreMoves = new ArrayList<>();
+
+        exploreMoves.addAll(gameState.getPlayers().stream()
+                .map(player -> new MoveImpl(player.getId(), Direction.NORTH))
+                .collect(Collectors.toList()));
+
+        System.out.println(exploreMoves.size() + " players exploring");
+        return exploreMoves;
     }
 
 
---
+++ src/main/java/com/contestantbots/util/MoveImpl.java
@@ -0,0 +1,22 @@
+package com.contestantbots.util;
+import com.scottlogic.hackathon.game.Direction;
+import com.scottlogic.hackathon.game.Move;
+
+import java.util.UUID;
+
+public class MoveImpl implements Move {
+    private UUID playerId;
+    private Direction direction;
+    public MoveImpl(UUID playerId, Direction direction) {
+        this.playerId = playerId;
+        this.direction = direction;
+    }
+    @Override
+    public UUID getPlayer() {
+        return playerId;
+    }
+    @Override
+    public Direction getDirection() {
+        return direction;
+    }
+}
